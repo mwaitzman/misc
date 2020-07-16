78a390e7e37adc7abe9563dcffcc8cc4d96f71cd
mwait101101 <59382740+mwait101101@users.noreply.github.com>
2020-07-16 12:27:44 -0400

    Create BetterBrowser.f

---
+++ BetterBrowser.f
@@ -0,0 +1,7 @@
+for example, I can set it so that typing ```r/<YOUR TERM> will go to reddit.com/r/<YOUR TERM>
+
+```lol jax``` will redirect to leagueoflegends.fandom.com/wiki/Jax
+
+``` lol jax guide``` will redirect to mobafire jax guide list
+can disable with flag override in the search
+can turn off in settings
